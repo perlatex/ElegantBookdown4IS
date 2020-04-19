3c1472846957f094a9246789b4641482049ff560
perlatex <wangmj@sicnu.edu.cn>
2020-04-19 15:30:08 +0800

    wrangle data

--- data/is_within.R
+++ data/is_within.R
@@ -5,9 +5,11 @@ is_within_univ <- function(df, var) {
 		c("Beijing Normal University", "East China Normal University", "Central China Normal University", "Nanjing Normal University", "Hunan Normal University", "Northeast Normal University", "South China Normal University", "Shaanxi Normal University", "Capital Normal University", "Zhejiang Normal University", "Shandong Normal University", "Tianjin Normal University", "Fujian Normal University", "Henan Normal University", "Jiangxi Normal University", "Shanghai Normal University", "Anhui Normal University", "Northwest Normal University", "Guangxi Normal University", "Hangzhou Normal University", "Yunnan Normal University", "Harbin Normal University", "Hebei Normal University", "Jiangsu Normal University", "Sichuan Normal University", "Liaoning Normal University", "Chongqing Normal University", "Qufu Normal University", "Guizhou Normal University", "Hainan Normal University")
 	
 	
-	df %>% mutate(
+	df %>% 
+		mutate(
 		is_within_univ = if_else({{var}} %in% alist, TRUE, FALSE)
-	)
+	) %>% 
+		filter(!is_within_univ)
 }
 
 
@@ -39,7 +41,8 @@ is_within_discipline <- function(df, var) {
 	
 	df %>% mutate(
 		is_within_discipline = if_else({{var}} %in% alist, TRUE, FALSE)
-	)
+	) %>% 
+		filter(!is_within_discipline)
 }
 
 
--- data/tidy_raw_data.R
+++ data/tidy_raw_data.R
@@ -1,142 +1,30 @@
 library(tidyverse)
+source("add_cn.R", encoding = "UTF-8")
 
 
 
 ######################################################
-# tidy university/discipline data from 2010-2019
-read_plus <- function(flnm) {
-  readr::read_csv(flnm,
-    col_types = list(
-      学科 = col_character(),
-      名称 = col_character(),
-      排名 = col_double(),
-      出版年 = col_double(),
-      `Web of Science 论文数` = col_double(),
-      学科规范化的引文影响力 = col_double(),
-      被引频次 = col_double(),
-      论文被引百分比 = col_double()
-    )
-  ) %>%
-    dplyr::rename(
-      discipline = `学科`,
-      univ = `名称`,
-      rank = `排名`,
-      year = `出版年`,
-      n_paper = `Web of Science 论文数`,
-      impact = `学科规范化的引文影响力`,
-      n_cited = `被引频次`,
-      percent_cited = `论文被引百分比`
-    ) %>%
-    dplyr::filter(!is.na(discipline)) %>%
-    dplyr::mutate(
-      school = flnm %>% stringr::str_extract(., "(?<=UnivTimeSerial/).*?(?=\\.csv)")
-    ) %>%
-    dplyr::select(school, discipline, year, n_paper, n_cited)
-}
+Normal_University_Top30 <- 
+	read_csv(here::here("data", "UniversityName", "schoolname.csv"))
+Normal_University_Top30
 
 
-univ_discip_timeserial <-
-  here::here("data", "UnivTimeSerial") %>%
-  fs::dir_ls(regexp = "*.csv", recurse = FALSE) %>%
-  purrr::map_dfr(~ read_plus(.)) %>%
-  dplyr::mutate(
-    discipline = dplyr::recode(discipline,
-      `Mathematics` = "数学",
-      `Physics` = "物理学",
-      `Chemistry` = "化学",
-      `Engineering` = "工程学",
-      `Computer Science` = "计算机科学",
-      `Materials Science` = "材料科学",
-      `Economics & Business` = "经济与商业",
-      `Psychiatry/Psychology` = "精神病学与心理学"
-    )
-  )
-univ_discip_timeserial
-
-
-univ_discip_timeserial %>%
-  filter(discipline != "ALL") %>%
-  filter(stringr::str_detect(discipline, "^A"))
-
-univ_discip_timeserial %>% count(school, discipline)
-
-univ_discip_timeserial %>%
-  filter(school == "北京师范大学") %>%
-  count(discipline)
-
-# here::here("data", "UnivTimeSerial", "北京师范大学.csv") %>% read_csv()
+Normal_University_Top30 %>% 
+	add_school_cn(univ)
 ######################################################
 
 
 
 
-
-
-
 ######################################################
-# tidy newest ESI Threshold
-esi22 <- readxl::read_excel(here::here("data", "ESI", "ESI_22.xlsx")) %>%
-  dplyr::mutate(discipline = stringr::str_to_title(discipline))
+# ESI22
+esi22 <- readxl::read_excel(here::here("data", "ThresholdESI", "ESI_22.xlsx")) %>%
+	dplyr::mutate(discipline = stringr::str_to_title(discipline))
 esi22
 
-esi22 %>%
-	is_within_discipline(discipline) %>% 
-	filter(!is_within_discipline)
 
-
-Threshold <-
-  readxl::read_excel(here::here("data", "ESI", "ThresholdESI0326.xlsx"),
-    skip = 2, n_max = 22
-  ) %>%
-  janitor::clean_names() %>%
-  dplyr::select(discipline = research_fields, Threshold0326 = institution) %>%
-  dplyr::mutate(discipline = stringr::str_to_title(discipline))
-Threshold
-
-
-
-ThresholdESI <- left_join(esi22, Threshold, by = "discipline")
-ThresholdESI
-
-ThresholdESI %>%
-    is_within_discipline(discipline) %>% 
-	filter(!is_within_discipline)
-######################################################
-
-
-
-
-
-
-######################################################
-# univ discipline development data.frame summary from last ten years
-# 2010-2020
-univ_discip_df <-
-  readxl::read_excel(here::here("data", "UnivSummary", "university_summary.xlsx")) %>%
-  dplyr::filter(!is.na(school)) %>%
-  dplyr::mutate(discipline = stringr::str_to_title(discipline))
-univ_discip_df
-
-univ_discip_df %>% 
-	is_within_discipline(discipline) %>% 
-	filter(!is_within_discipline)
-
-
-
-univ_discip_stat <- univ_discip_df %>%
-  dplyr::left_join(
-    ThresholdESI %>% select(discipline, discipline_cn, Threshold0326),
-    by = "discipline"
-  ) %>%
-  dplyr::select(school, univ, discipline_cn, discipline, cum_paper, cum_cited, Threshold0326)
-
-univ_discip_stat
-univ_discip_stat %>% dplyr::filter(is.na(discipline_cn)) # have 5 NA
-
-
-univ_discip_stat %>% 
-	is_within_discipline(discipline) %>% 
-	filter(!is_within_discipline)
+t <- esi22 %>% add_esi_threshold(discipline)
+t
 ######################################################
 
 
@@ -146,21 +34,62 @@ univ_discip_stat %>%
 
 
 ######################################################
+# tidy university30/discipline22/TimeSerial21 data from 2000-2020last
+read_plus <- function(flnm) {
+  readr::read_csv(flnm) %>%
+    dplyr::select(
+      discipline = "名称",
+      year = "出版年",
+      n_paper = "Web of Science 论文数",
+      n_cited = "被引频次"
+    ) %>%
+    dplyr::filter(!is.na(year)) %>%
+	dplyr::mutate(discipline = stringr::str_to_title(discipline)) %>%
+    dplyr::mutate(
+      univ = flnm %>% stringr::str_extract(., "(?<=UnivDisciplineTimeSerial/).*?(?=\\.csv)")
+    )
+}
+
+univ_discip_timeserial <-
+  here::here("data", "UnivDisciplineTimeSerial") %>%
+  fs::dir_ls(regexp = "*.csv", recurse = FALSE) %>%
+  purrr::map_dfr(~ read_plus(.)) %>%
+  dplyr::select(univ, discipline, year, n_paper, n_cited)
+
+
+univ_discip_timeserial
+univ_discip_timeserial %>% count(univ)
+
+colnames(univ_discip_timeserial)
+
+
+univ_discip_timeserial %>% 
+	add_discipline_cn(discipline) %>% 
+	add_school_cn(univ)
+
+##########################################################################
+
+
+
+
+
+
+
+##########################################################################
 # tidy top one percent
 read_top <- function(flnm) {
   readxl::read_excel(flnm, skip = 5) %>%
     janitor::clean_names() %>%
-    dplyr::filter(!is.na(institutions)) %>%
-    filter(stringr::str_detect(countries_regions, "CHINA")) %>%
-    filter(stringr::str_detect(institutions, "NORMAL")) %>%
+    dplyr::filter(!is.na(research_fields)) %>%
+    dplyr::filter(research_fields != "ALL FIELDS") %>%
+    dplyr::mutate(discipline = stringr::str_to_title(research_fields)) %>%
+    dplyr::mutate(is_enter_one_percent = TRUE) %>%
     dplyr::mutate(
-      discipline = flnm %>% stringr::str_extract(., "(?<=TopOnePercent/).*?(?=\\.xlsx)")
+      univ = flnm %>% stringr::str_extract(., "(?<=TopOnePercent/).*?(?=\\.xlsx)")
     ) %>%
-    mutate(is_enter_one_percent = TRUE) %>%
-    mutate_at(vars(institutions), ~ stringr::str_extract(., ".*?UNIVERSITY")) %>%
-    mutate_at(vars(institutions), ~ stringr::str_to_title(.)) %>%
-    select(discipline,
-      univ = institutions, web_of_science_documents,
+    select(
+      univ, discipline,
+      web_of_science_documents,
       cites, top_papers, is_enter_one_percent
     )
 }
@@ -169,39 +98,22 @@ read_top <- function(flnm) {
 top_one_percent <-
   here::here("data", "TopOnePercent") %>%
   fs::dir_ls(regexp = "*.xlsx", recurse = FALSE) %>%
-  purrr::map_dfr(~ read_top(.)) %>%
-  dplyr::mutate(
-    discipline = dplyr::recode(discipline,
-      `Environment_Ecology` = "Environment/Ecology",
-      `Psychiatry_Psychology` = "Psychiatry/Psychology",
-      `Social Sciences, general` = "Social Sciences, General"
-    )
-  )
+  purrr::map_dfr(~ read_top(.))
 
 top_one_percent
 top_one_percent %>% 
-	count(univ) %>% 
-	arrange(-n)
-
-top_one_percent %>%
-    is_within_discipline(discipline) %>% 
-	filter(!is_within_discipline)
+	add_discipline_cn(discipline) %>% 
+	add_school_cn(univ)
 
 
-univ_discip_summary <- univ_discip_stat %>%
-  dplyr::left_join(top_one_percent, by = c("discipline", "univ"))
-univ_discip_summary
-
-
-univ_discip_summary %>% 
-	group_by(school) %>% 
-	summarise( 
-		num = sum(is_enter_one_percent, na.rm = T)
-	) %>% 
-	arrange(desc(num))
-
-######################################################
-
+t <- top_one_percent %>%
+  count(discipline) %>%
+  arrange(-n) %>%
+  add_discipline_cn(discipline) #%>% 
+  #rename(dd = discipline) #%>% 
+t
+t %>% add_esi_threshold(discipline)
+##########################################################################
 
 
 
@@ -212,21 +124,67 @@ univ_discip_summary %>%
 
 
 ######################################################
+# wrangle for save
+# 1)  school / univ
+Normal_University_Top30
+
+
+# 2) category / discipline_mix / discipline_cn /  discipline / Threshold0326
+ESI_and_Threshold <- esi22 %>% add_esi_threshold(discipline)
+
+
+# 3) univ / discipline / year / n_paper / n_cited
+univ_discip_timeserial_2000_to_2019 <- 
+	univ_discip_timeserial %>% 
+	filter(between(year, 2000, 2019)) %>% 
+	add_discipline_cn(discipline) %>% 
+	add_school_cn(univ)
+
+
+# 4) univ / discipline / year / n_paper / n_cited
+univ_discip_timeserial_2010_to_2019 <- 
+	univ_discip_timeserial %>% 
+	filter(between(year, 2010, 2019)) %>% 
+	add_discipline_cn(discipline) %>% 
+	add_school_cn(univ)
+
+
+
+
+# 5) univ / discipline / is_enter_one_percent
+univ_discip_enter_top_one_percent <- top_one_percent %>% 
+	add_discipline_cn(discipline) %>% 
+	add_school_cn(univ)
+
+
+
+# 6) univ / discipline / year / n_paper / n_cited
+univ_discip_cum_last_ten_year <- univ_discip_timeserial %>%
+	filter(between(year, 2010, 2020)) %>% # last ten year
+	group_by(univ, discipline) %>%
+	summarise(
+		cum_paper = sum(n_paper),
+		cum_cited = sum(n_cited)
+	) %>%
+	ungroup() %>% 
+	left_join(top_one_percent, by = c("discipline", "univ")) %>% 
+	add_esi_threshold(discipline) %>% 
+	add_discipline_cn(discipline) %>% 
+	add_school_cn(univ)
+
+
+
+
 # save to Rdata
-save(univ_discip_timeserial, ThresholdESI, univ_discip_summary,
-  file = "myData.Rdata"
+save(Normal_University_Top30,
+	 ESI_and_Threshold,
+	 univ_discip_timeserial_2000_to_2019, 
+	 univ_discip_timeserial_2010_to_2019, 
+	  
+	 univ_discip_enter_top_one_percent,
+	 univ_discip_cum_last_ten_year,
+	 file = "myData.Rdata"
 )
 
 # load("myData.Rdata")
-######################################################
-
-
-
-
-
-######################################################
-# backup
-univ_discip_stat %>%
-  distinct(univ) %>%
-  dplyr::mutate(univ = stringr::str_to_title(univ))
 ######################################################
