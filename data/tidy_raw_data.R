fceb5e24c612f002aec3b9e58d9ec669e0525c02
perlatex <wangmj@sicnu.edu.cn>
2020-06-04 11:16:18 +0800

    add highcited

--- data/tidy_raw_data.R
+++ data/tidy_raw_data.R
@@ -1,18 +1,16 @@
 library(tidyverse)
-source("add_cn.R", encoding = "UTF-8")
-source("map_issn_to_discipline.R", encoding = "UTF-8")
-source("recode_college_name.R", encoding = "UTF-8")
+library(tidyESI)
 
 
 
 ######################################################
 Normal_University_Top30 <- 
-	read_csv(here::here("data", "UniversityName", "schoolname.csv"))
+	readr::read_csv(here::here("data", "UniversityName", "schoolname.csv"))
 Normal_University_Top30
 
 
 Normal_University_Top30 %>% 
-	add_school_cn(univ)
+	tidyESI::add_univ_name_cn(univ)
 ######################################################
 
 
@@ -25,7 +23,7 @@ esi22 <- readxl::read_excel(here::here("data", "ThresholdESI", "ESI_22.xlsx")) %
 esi22
 
 
-t <- esi22 %>% add_esi_threshold(discipline)
+t <- esi22 %>% tidyESI::add_esi_threshold(discipline, date = "last")
 t
 ######################################################
 
@@ -65,8 +63,8 @@ colnames(univ_discip_timeserial)
 
 
 univ_discip_timeserial %>% 
-	add_discipline_cn(discipline) %>% 
-	add_school_cn(univ)
+	tidyESI::add_discipline_cn(discipline) %>% 
+	tidyESI::add_univ_name_cn(univ)
 
 ##########################################################################
 
@@ -103,17 +101,17 @@ top_one_percent <-
 
 top_one_percent
 top_one_percent %>% 
-	add_discipline_cn(discipline) %>% 
-	add_school_cn(univ)
+    tidyESI::add_discipline_cn(discipline) %>% 
+	tidyESI::add_univ_name_cn(univ)
 
 
 t <- top_one_percent %>%
-  count(discipline) %>%
-  arrange(-n) %>%
-  add_discipline_cn(discipline) #%>% 
-  #rename(dd = discipline) #%>% 
+  dplyr::count(discipline) %>%
+  dplyr::arrange(-n) %>%
+  tidyESI::add_discipline_cn(discipline) 
+
 t
-t %>% add_esi_threshold(discipline)
+t %>% tidyESI::add_esi_threshold(discipline)
 ##########################################################################
 
 
@@ -122,7 +120,7 @@ t %>% add_esi_threshold(discipline)
 
 ##########################################################################
 read_wos <- function(flnm) {
-  read_tsv(flnm, quote = "", col_names = TRUE) %>% 
+    read_tsv(flnm, quote = "", col_names = TRUE) %>% 
     select(AU, AF, SO, DE, C1, RP, FU, CR, TC, SN, PY, UT) 
 }
 
@@ -134,12 +132,12 @@ tbl
 
 t <- tbl %>% 
   dplyr::select(C1, TC, SN) %>% 
-  dplyr::mutate(discipline = map_issn_to_discipline(SN)) %>% 
+  dplyr::mutate(discipline = tidyESI::map_issn_to_esi_discipline(SN)) %>% 
   rowwise() %>% 
   dplyr::mutate(coll = stringr::str_extract_all(C1, "Sichuan Normal Univ,\\s+([^,]*),") 
   ) %>% 
   tidyr::unnest(coll) %>% 
-  ungroup()
+  dplyr::ungroup()
 
 
 sicnu_contribution_by_college <- t %>% 
@@ -147,7 +145,7 @@ sicnu_contribution_by_college <- t %>%
   dplyr::mutate(
     across(coll, stringr::str_squish)
   ) %>%
-  dplyr::mutate(coll_name_cn = recode_college_name(coll)) %>% 
+  dplyr::mutate(coll_name_cn = tidyESI::map_addr_to_college_name(coll)) %>% 
   dplyr::filter(!is.na(coll_name_cn), !is.na(discipline)) %>% 
   dplyr::group_by(discipline, coll_name_cn) %>% 
   dplyr::summarise(
@@ -156,9 +154,43 @@ sicnu_contribution_by_college <- t %>%
     .groups = "drop"
   ) %>% 
   dplyr::arrange(-n_cited)  %>% 
-  add_discipline_cn(discipline) %>% 
+  tidyESI::add_discipline_cn(discipline) %>% 
   dplyr::relocate(discipline_cn)
 ##########################################################################
+
+
+
+
+
+##########################################################################
+highcited_timeserial <- 
+	here::here("data","top_paper", "top_paper.xlsx") %>%
+	readxl::read_excel(., skip = 1) %>%
+	janitor::clean_names() %>%
+	dplyr::filter(stringr::str_detect(accession_number, "^WOS")) %>% 
+	dplyr::mutate(discipline = stringr::str_to_title(research_field)) %>%
+	dplyr::mutate(times_cited = as.numeric(times_cited)) %>%
+	dplyr::select(
+		discipline, 
+		year = publication_date,
+		times_cited
+	)  %>%  
+	dplyr::group_by(discipline, year) %>% 
+	dplyr::summarise(
+		n_paper_high = n(),
+		n_cited_high = sum(times_cited),
+		.groups = "drop"
+	) 
+highcited_timeserial
+
+
+
+##########################################################################
+
+
+
+
+
 
 
 
@@ -172,51 +204,61 @@ Normal_University_Top30
 
 
 # 2) category / discipline_mix / discipline_cn /  discipline / Threshold0326
-ESI_and_Threshold <- esi22 %>% add_esi_threshold(discipline)
+ESI_and_Threshold <- esi22 %>% tidyESI::add_esi_threshold(discipline)
 
 
 # 3) univ / discipline / year / n_paper / n_cited
 univ_discip_timeserial_2000_to_2019 <- 
 	univ_discip_timeserial %>% 
-	filter(between(year, 2000, 2019)) %>% 
-	add_discipline_cn(discipline) %>% 
-	add_school_cn(univ)
+	dplyr::filter(between(year, 2000, 2019)) %>% 
+	tidyESI::add_discipline_cn(discipline) %>% 
+	tidyESI::add_univ_name_cn(univ)
 
 
-# 4) univ / discipline / year / n_paper / n_cited
+
 univ_discip_timeserial_2010_to_2019 <- 
 	univ_discip_timeserial %>% 
-	filter(between(year, 2010, 2019)) %>% 
-	add_discipline_cn(discipline) %>% 
-	add_school_cn(univ)
+	dplyr::filter(between(year, 2010, 2019)) %>% 
+	tidyESI::add_discipline_cn(discipline) %>% 
+	tidyESI::add_univ_name_cn(univ)
+
+
+univ_discip_timeserial_2010_to_2020 <- 
+	univ_discip_timeserial %>% 
+	dplyr::filter(between(year, 2010, 2020)) %>% 
+	tidyESI::add_discipline_cn(discipline) %>% 
+	tidyESI::add_univ_name_cn(univ)
 
 
 
-
-# 5) univ / discipline / is_enter_one_percent
+# 4) univ / discipline / is_enter_one_percent
 univ_discip_enter_top_one_percent <- top_one_percent %>% 
-	add_discipline_cn(discipline) %>% 
-	add_school_cn(univ)
+	tidyESI::add_discipline_cn(discipline) %>% 
+	tidyESI::add_univ_name_cn(univ)
 
 
 
-# 6) univ / discipline / year / n_paper / n_cited
+# 5) univ / discipline / year / n_paper / n_cited
 univ_discip_cum_last_ten_year <- univ_discip_timeserial %>%
-	filter(between(year, 2010, 2020)) %>% # last ten year
-	group_by(univ, discipline) %>%
-	summarise(
+	dplyr::filter(between(year, 2010, 2020)) %>% # last ten year
+	dplyr::group_by(univ, discipline) %>%
+	dplyr::summarise(
 		cum_paper = sum(n_paper),
 		cum_cited = sum(n_cited)
 	) %>%
-	ungroup() %>% 
-	left_join(top_one_percent, by = c("discipline", "univ")) %>% 
-	add_esi_threshold(discipline) %>% 
-	add_discipline_cn(discipline) %>% 
-	add_school_cn(univ)
+	dplyr::ungroup() %>% 
+	dplyr::left_join(top_one_percent, by = c("discipline", "univ")) %>% 
+	tidyESI::add_esi_threshold(discipline) %>% 
+	tidyESI::add_discipline_cn(discipline) %>% 
+	tidyESI::add_univ_name_cn(univ)
 
 
-# 7) sichuan normal univ: (college) / (discipline) / (n_paper) / (n_cited)
+# 6) sichuan normal univ: (college) / (discipline) / (n_paper) / (n_cited)
 sicnu_contribution_by_college
+
+
+# 7) sichuan normal univ: highcited_timeserial
+highcited_timeserial
 
 
 
@@ -225,11 +267,14 @@ save(Normal_University_Top30,
 	 ESI_and_Threshold,
 	 univ_discip_timeserial_2000_to_2019, 
 	 univ_discip_timeserial_2010_to_2019, 
+	 univ_discip_timeserial_2010_to_2020,
 	  
 	 univ_discip_enter_top_one_percent,
 	 univ_discip_cum_last_ten_year,
 	 
 	 sicnu_contribution_by_college,
+	 
+	 highcited_timeserial,
 	 file = "myData.Rdata"
 )
 
